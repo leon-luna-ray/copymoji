60310c6477cca6dc3484f50e489d232637b84a2e
Ray Luna <ray@hello-upswell.com>
2023-09-28 17:57:51 -0700

    Initial commit

---
+++ src/components/EmojiGrid.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container grid grid-cols-10">
+        <div v-for="emoji in emojis.nature" class="text-[3rem] hover:cursor-pointer" @click="copyToClipboard(emoji)"><span>
+                {{ emoji }}
+            </span>
+        </div>
+    </div>
+    <div>Copied to clipboard!</div>
+</template>
+
+<script setup>
+import { emojis } from '../lib/emojis'
+
+const copyToClipboard = (text) => {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+}
+</script>
